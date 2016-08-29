ebc9e82be72a64357ecbc9a5ba4247e89c29e48f
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2016-08-29 11:02:22 +0200

    add some documentation for the Faraday Class

--- src/Faraday/faraday.cpp
+++ src/Faraday/faraday.cpp
@@ -3,7 +3,11 @@
 #include "faradayfactory.h"
 #include "grid/gridlayout.h"
 
-
+/**
+ * @brief Faraday::Faraday builds a Faraday object from the time step and the grid layout
+ * @param dt : time step
+ * @param layout : GridLayout
+ */
 Faraday::Faraday(double dt, GridLayout const& layout)
     : implPtr_{FaradayImplFactory::createFaradayImpl(dt, layout)}
 {
--- src/Faraday/faraday.h
+++ src/Faraday/faraday.h
@@ -11,7 +11,9 @@
 
 
 
-// comment here
+/**
+ * @brief Interface for concrete Faraday implementations (1D, 2D, 3D)
+ */
 class FaradayImpl
 {
     public:
@@ -22,6 +24,13 @@ class FaradayImpl
 
 
 
+
+/**
+ * @brief The FaradayImplInternals class factorizes details of implementation
+ *  for the concrete derived classes inheriting from FaradayImpl.
+ *
+ *  The class encapsulates the time step and the GridLayout
+ */
 class FaradayImplInternals
 {
 public:
@@ -36,24 +45,29 @@ protected:
 
 
 
-// comment here
+/**
+ * @brief The Faraday class represents the faraday equation solver.
+ *
+ * Faraday provides client code with an operator() taking the old magnetic field,
+ * the electric field and a reference to the updated magnetic field vector.
+ * The class encapsulate a unique pointer to an abstract implementation of the
+ * solver, which is realized by concrete implementations knowing the dimensionality etc.
+ *
+ * Faraday is intended to be part of a Solver object and is therefore move-only.
+ */
 class Faraday
 {
 private:
-
-    //
-
 
     std::unique_ptr<FaradayImpl> implPtr_;
 
 
 public:
 
-    // TODO : faraday object should take start and end "iterators"
-    // those are given by SOlver which takes them from GridLayout
+
     explicit Faraday(double dt, GridLayout const& layout);
 
-
+    // Faraday is a move-only object.
     Faraday(Faraday const& source) = delete;
     Faraday& operator=(Faraday const& source) = delete;
 
