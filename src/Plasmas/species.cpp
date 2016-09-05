5828a0a41980872dfa162528bb9c65e87cb80a77
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2016-09-05 23:10:10 +0200

    add Species constructors

--- src/Plasmas/species.cpp
+++ src/Plasmas/species.cpp
@@ -1,6 +1,19 @@
 #include "species.h"
 
-Species::Species()
+Species::Species(GridLayout&& layout, std::string const& name)
+
+    : layout_{ std::move(layout) },
+      rho_{ layout_.nx(), layout_.ny(), layout_.nz(), "rho_"+name },
+      bulkVel_{ layout_.nx(), layout_.ny(), layout_.nz(),"bulkVel_"+name }
+{
+
+}
+
+
+Species:: Species(GridLayout const& layout, std::string const& name)
+    : layout_{ layout },
+      rho_    { layout_.nx(), layout_.ny(), layout_.nz(), "rho_"   + name },
+      bulkVel_{ layout_.nx(), layout_.ny(), layout_.nz(),"bulkVel_"+ name }
 {
 
 }
--- src/Plasmas/species.h
+++ src/Plasmas/species.h
@@ -1,16 +1,33 @@
 #ifndef SPECIES_H
 #define SPECIES_H
 
+#include "Field/field.h"
+#include "grid/gridlayout.h"
 
-class Field;
-class Particles;
 
 class Species
 {
+
+private:
+    GridLayout layout_;
+    Field rho_;
+    Field bulkVel_;
+
 public:
-    Species();
+    explicit Species(GridLayout const& layout, std::string const& name);
+    explicit Species(GridLayout&& layout, std::string const& name);
+
+    Species(Species const& source) = delete;
+    Species& operator=(Species const& source)=delete;
+
+    Species(Species&& source) = default;
+    Species& operator=(Species&& source) = default;
+
     //~Species();
 
+    void resetMoments() {rho_.zero(); bulkVel_.zero();}
+
+#if 0
     Field* getChargeDensity();
     Field* getBulkVelocity();
     Field* getFLux();
@@ -26,6 +43,8 @@ private:
     Field* _v;
     Field* _flux;
     Particles* _particle;
+#endif
+
 };
 
 #endif // SPECIES_H
