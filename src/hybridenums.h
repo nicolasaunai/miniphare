95f678b3a4d0dbd31732533e9bbf37d1af171527
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2016-09-15 18:21:08 +0200

    add enum counter in HybridQuantity

--- src/hybridenums.h
+++ src/hybridenums.h
@@ -2,7 +2,7 @@
 #define HYBRIDENUMS_H
 
 
-enum class HybridQuantity{ Bx, By, Bz, Ex, Ey, Ez, rho, V, P, Ohm, temporary};
+enum class HybridQuantity{ Bx, By, Bz, Ex, Ey, Ez, rho, V, P, count, temporary};
 
 enum class OhmTerm{ Ideal, Pressure, Resistivity, HyperResistivity };
 
