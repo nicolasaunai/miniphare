43eec9ee4223db80816a59c625cbca729adf8d82
droudrou <mathieu.drouin@obspm.fr>
2016-09-27 18:34:04 +0200

    Python script fieldNodeCoordinates.py was clarified in order to test method: Point GridLayout::fieldNodeCoordinates(...)

--- tests/GridLayout/fieldNodeCoordinates.py
+++ tests/GridLayout/fieldNodeCoordinates.py
@@ -18,171 +18,26 @@ import math
 #from math import factorial
 #%matplotlib inline
 
-
 nbPadding = [10,10,10]
+
+Direction_l = [(0,'X'), (1,'Y'), (2,'Z')]
+
+Qty_l=[(0,'Bx'), (1,'By'), (2,'Bz'), (3,'Ex'), (4,'Ey'), (5,'Ez'), (6,'rho'), (7,'V'), (8,'P')]
+
 QtyDict = {'Bx':0, 'By':1, 'Bz':2, 'Ex':3, 'Ey':4, 'Ez':5, 'rho':6, 'V':7, 'P':8}
 
+CenterX = {'Bx':'primal' , 'By':'dual'  , 'Bz':'dual'  , 
+           'Ex':'dual'   , 'Ey':'primal', 'Ez':'primal', 
+           'rho':'primal', 'V':'primal' , 'P':'primal' }
 
-# ---------------------- X DIRECTION -----------------------------------------
+CenterY = {'Bx':'dual'   , 'By':'primal', 'Bz':'dual'  , 
+           'Ex':'primal' , 'Ey':'dual'  , 'Ez':'primal', 
+           'rho':'primal', 'V':'primal' , 'P':'primal' }
+           
+CenterZ = {'Bx':'dual'   , 'By':'dual'  , 'Bz':'primal', 
+           'Ex':'primal' , 'Ey':'primal', 'Ez':'dual'  , 
+           'rho':'primal', 'V':'primal' , 'P':'primal' }
 
-# ---- usefull methods ------
-def nbrGhostsX(interpOrder, qty):
-    if qty[0] in (QtyDict['Ex'], QtyDict['By'], QtyDict['Bz']) :# dual qties
-        return math.floor( (interpOrder +1)/2 )
-    else: # primal
-        return math.floor( interpOrder/2 )
-
-def nbrGhostsDerivedX(interpOrder, qty):
-    # once derived, they become primal qties
-    if qty[0] in (QtyDict['Ex'], QtyDict['By'], QtyDict['Bz']) :
-        return math.floor( interpOrder/2 )
-    else: # once derived, they become dual qties
-        return math.floor( (interpOrder +1)/2 )
-
-        
-def isDualX(qty):
-    if qty[0] in (QtyDict['Ex'], QtyDict['By'], QtyDict['Bz']) :# dual qties
-        return 1
-    else:
-        return 0
-    
-# ---- Start / End index methods ------
-def ghostStartIndexX():
-    return 0
-
-def physicalStartPrimalX(interpOrder):
-    return ghostStartIndexX() + math.floor( interpOrder/2 )
-
-def physicalEndPrimalX(interpOrder, nbrCells):
-    return physicalStartPrimalX(interpOrder) + nbrCells
-
-def physicalStartIndexX(interpOrder, nbrCells, qty):
-    return ghostStartIndexX() + nbrGhostsX(interpOrder, qty)
-
-def physicalEndIndexX(interpOrder, nbrCells, qty):
-    return physicalStartIndexX(interpOrder, nbrCells, qty) + nbrCells - isDualX(qty)
-
-def ghostEndIndexX(interpOrder, nbrCells, qty):
-    return physicalEndIndexX(interpOrder, nbrCells, qty) + nbrGhostsX(interpOrder, qty)
-
-#    uint32 nx =  2*nbrPaddingCells( Direction::X ) + nbrPhysicalCells( Direction::X ) + 1
-#               + 2*nbrGhosts( qtyCenterings[data.idirX] ) ;
-def allocSizeX(interpOrder, nbrCells, qty):
-    return nbrCells + 1 + 2*nbrGhostsX(interpOrder, qty)
-
-def allocSizeDerivedX(interpOrder, nbrCells, qty):
-    return nbrCells + 1 + 2*nbrGhostsDerivedX(interpOrder, qty)
-
-
-def fieldCoordsX(iprimal, ixStart, qty, dx, origin):
-
-    halfCell = 0.    
-    if qty[0] in (QtyDict['Ex'], QtyDict['By'], QtyDict['Bz']) :# dual qties
-        halfCell = 0.5 
-        
-    x = ( (iprimal - ixStart) + halfCell )*dx + origin[0]
-
-    return x
-
-
-# ---------------------- Y DIRECTION -----------------------------------------
-    
-# ---- usefull methods ------
-def nbrGhostsY(interpOrder, qty):
-    if qty[0] in (QtyDict['Ey'], QtyDict['Bx'], QtyDict['Bz']) :# dual qties
-        return math.floor( (interpOrder +1)/2 )
-    else: # primal
-        return math.floor( interpOrder/2 )
-        
-def nbrGhostsDerivedY(interpOrder, qty):
-    # once derived, they become primal qties
-    if qty[0] in (QtyDict['Ey'], QtyDict['Bx'], QtyDict['Bz']) :
-        return math.floor( interpOrder/2 )
-    else: # once derived, they become dual qties
-        return math.floor( (interpOrder +1)/2 )        
-        
-def isDualY(qty):
-    if qty[0] in (QtyDict['Ey'], QtyDict['Bx'], QtyDict['Bz']) :# dual qties
-        return 1
-    else:
-        return 0
-    
-# ---- Start / End index methods ------
-def ghostStartIndexY():
-    return 0
-
-def physicalStartIndexY(interpOrder, nbrCells, qty):
-    return ghostStartIndexY() + nbrGhostsY(interpOrder, qty)
-
-def physicalEndIndexY(interpOrder, nbrCells, qty):
-    return physicalStartIndexY(interpOrder, nbrCells, qty) + nbrCells - isDualY(qty)
-
-def ghostEndIndexY(interpOrder, nbrCells, qty):
-    return physicalEndIndexY(interpOrder, nbrCells, qty) + nbrGhostsY(interpOrder, qty)
-
-
-def allocSizeY(interpOrder, nbrCells, qty):
-    return nbrCells + 1 + 2*nbrGhostsY(interpOrder, qty)
-
-def allocSizeDerivedY(interpOrder, nbrCells, qty):
-    return nbrCells + 1 + 2*nbrGhostsDerivedY(interpOrder, qty)
-
-# ---------------------- Z DIRECTION -----------------------------------------
-
-# ---- usefull methods ------
-def nbrGhostsZ(interpOrder, qty):
-    if qty[0] in (QtyDict['Ez'], QtyDict['Bx'], QtyDict['By']) :# dual qties
-        return math.floor( (interpOrder +1)/2 )
-    else: # primal
-        return math.floor( interpOrder/2 )
-
-        
-def nbrGhostsDerivedZ(interpOrder, qty):
-    # once derived, they become primal qties
-    if qty[0] in (QtyDict['Ez'], QtyDict['Bx'], QtyDict['By']) :
-        return math.floor( interpOrder/2 )
-    else: # once derived, they become dual qties
-        return math.floor( (interpOrder +1)/2 )                   
-        
-def isDualZ(qty):
-    if qty[0] in (QtyDict['Ez'], QtyDict['Bx'], QtyDict['By']) :# dual qties
-        return 1
-    else:
-        return 0
-    
-# ---- Start / End index methods ------
-def ghostStartIndexZ():
-    return 0
-
-def physicalStartIndexZ(interpOrder, nbrCells, qty):
-    return ghostStartIndexZ() + nbrGhostsZ(interpOrder, qty)
-
-def physicalEndIndexZ(interpOrder, nbrCells, qty):
-    return physicalStartIndexZ(interpOrder, nbrCells, qty) + nbrCells - isDualZ(qty)
-
-def ghostEndIndexZ(interpOrder, nbrCells, qty):
-    return physicalEndIndexZ(interpOrder, nbrCells, qty) + nbrGhostsZ(interpOrder, qty)
-
-
-def allocSizeZ(interpOrder, nbrCells, qty):
-    return nbrCells + 1 + 2*nbrGhostsZ(interpOrder, qty)
-
-def allocSizeDerivedZ(interpOrder, nbrCells, qty):
-    return nbrCells + 1 + 2*nbrGhostsDerivedZ(interpOrder, qty)
-
-# ---------------------- MAIN CODE -----------------------------------------
-
-paramList = ['interpOrder', 'nbDims', 'Quantity', 'nbrCellX', 'nbrCellY', 'nbrCellZ', 'dx', 'dy', 'dz'];
-len( paramList )
-print( paramList[:] )
-print( paramList[2] )
-
-interpOrder_l=[1, 2, 3, 4]
-nbDims_l=[1] 
-
-#Qty_l=['Bx', 'By', 'Bz', 'Ex', 'Ey', 'Ez', 'rho', 'V', 'P']
-Qty_l=[0, 1, 2, 3, 4, 5, 6, 7, 8]
-Qty_l=[(0,'Bx'), (1,'By'), (2,'Bz'), (3,'Ex'), (4,'Ey'), (5,'Ez'), (6,'rho'), (7,'V'), (8,'P')]
 
 nbrCellX_l=[40, 40, 40]
 nbrCellY_l=[ 0, 12, 12]
@@ -193,10 +48,115 @@ dy_l=[0. , 0.1, 0.1]
 dz_l=[0. , 0. , 0.1]
 
 origin = [0., 0., 0.]
+           
+
+# ---------------------- ANY DIRECTION -----------------------------------------
+def qtyCentering(qty, direct):
+    if direct == 'X':
+        return CenterX[qty]
+    elif direct == 'Y':
+        return CenterY[qty]
+    elif direct == 'Z':
+        return CenterZ[qty]
+
+def nbrGhosts(interpOrder, qty, direct):
+    if qtyCentering(qty, direct) == 'primal':
+        return math.floor( interpOrder/2 )
+    else:
+        return math.floor( (interpOrder +1)/2 )
+
+def nbrGhostsPrimal(interpOrder):
+    return math.floor( interpOrder/2 )
+
+def isDual(qty, direct):
+    if qtyCentering(qty, direct) == 'dual':
+        return 1
+    else:
+        return 0
+        
+def nbrCells(direction, icas):
+    if direction == 'X':
+        return nbrCellX_l[icas]
+    elif direction == 'Y':
+        return nbrCellY_l[icas]
+    elif direction == 'Z':
+        return nbrCellZ_l[icas]
+
+def spatialStep(direction, icas):
+    if direction == 'X':
+        return dx_l[icas]
+    elif direction == 'Y':
+        return dy_l[icas]
+    elif direction == 'Z':
+        return dz_l[icas]
+
+# ---- Start / End index methods ------
+def ghostStartIndex():
+    return 0
+
+def physicalStartIndex(interpOrder, qty, direction):
+    index = ghostStartIndex() + nbrGhosts(interpOrder, qty, direction) 
+    return index
+
+def physicalEndIndex(interpOrder, qty, direction, caseNbr):
+    index = physicalStartIndex(interpOrder, qty, direction) \
+    + nbrCells(direction, caseNbr) - isDual(qty, direction)    
+    return index
+
+def ghostEndIndex(interpOrder, qty, direction, caseNbr):
+    index = physicalEndIndex(interpOrder, qty, direction, caseNbr) \
+    + nbrGhosts(interpOrder, qty, direction)       
+    return index
+    
+def physicalStartPrimal(interpOrder):
+    index = ghostStartIndex() + nbrGhostsPrimal(interpOrder)     
+    return index
+
+def physicalEndPrimal(interpOrder, direction, caseNbr):
+    index = physicalStartPrimal(interpOrder) + nbrCells(direction, caseNbr)    
+    return index
+
+# ---- Alloc methods -------------------------
+
+def allocSize(interpOrder, qty, direction, caseNbr):
+    size = nbrCells(direction, caseNbr) + 1 \
+    + 2*nbrGhosts(interpOrder, qty, direction)    
+    
+    return size
+
+
+# ---- Get coordinate methods -------------------------
+# ds stands for dx or dy or dz
+#
+# This method returns a point
+#
+def fieldCoords(iprimal, iStart, qty, direction, ds, origin):
+    halfCell = 0.    
+        
+    if qtyCentering(qty, direction[1]) == 'dual':
+        halfCell = 0.5 
+        
+    x = ( (iprimal - iStart) + halfCell )*ds + origin[direction[0]]
+
+    return x
+
+
+# ---------------------- MAIN CODE -----------------------------------------
+
+paramList = ['interpOrder', 'nbDims', 'Quantity', 'nbrCellX', 'nbrCellY', 'nbrCellZ', 'dx', 'dy', 'dz'];
+len( paramList )
+print( paramList[:] )
+print( paramList[2] )
+
+interpOrder_l=[1, 2, 3, 4]
+case_l=[0] 
+#dim_l =[0, 1, 2] 
+dim_l =[0] 
+
 
 maxNbrDim = 3
 
-nbrTestCases = len(interpOrder_l)*len(nbDims_l)*len(Qty_l)
+nbrTestCases = len(interpOrder_l)*len(dim_l)*len(Qty_l)*len(case_l)
 print( nbrTestCases )
 
 caseParamsTable = np.zeros((nbrTestCases, len(paramList) + 2*maxNbrDim))
@@ -207,41 +167,60 @@ print( col_l )
 
 iord_l=np.arange( len(interpOrder_l) ) 
 print( iord_l )
-idim_l=np.arange( len(nbDims_l) ) 
-print( idim_l )
+
 iqty_l=np.arange( len(Qty_l) ) 
 print( iqty_l )
 
+# ------- Debug commands -------
+for icase in case_l:
+    for idim in dim_l:
+        for iqty in iqty_l:
+            print( "field : %s, direction : %s, centering : %s" % 
+                  (Qty_l[iqty][1], Direction_l[idim][1], 
+                   qtyCentering(Qty_l[iqty][1], Direction_l[idim][1]) ) )  
+                   
+            print( "Nbr of cells = %d" % nbrCells(Direction_l[idim][1], icase) )
+                   
+            print( "Nbr of ghost cells = %d" % 
+            nbrGhosts(interpOrder_l[0], Qty_l[iqty][1], Direction_l[idim][1]) )
+            
+            print( "Alloc size = %d" %
+            allocSize(interpOrder_l[0], Qty_l[iqty][1], Direction_l[idim][1], icase) )
+            
+            print( physicalStartIndex(interpOrder_l[0], Qty_l[iqty][1], Direction_l[idim][1]),
+                   physicalEndIndex(interpOrder_l[0], Qty_l[iqty][1], Direction_l[idim][1], icase), 
+                   ghostEndIndex(interpOrder_l[0], Qty_l[iqty][1], Direction_l[idim][1], icase)  )
+# ------------------------------
 
 
 for iord in iord_l:
-    for idim in idim_l:
-        for iqty in iqty_l:
-            f = open(("fieldCoords_ord%d_dim%d_%s.txt") % 
-            (interpOrder_l[iord], nbDims_l[idim], Qty_l[iqty][1]), "w")
+    for icase in case_l:
+        for idim in dim_l:
+            for iqty in iqty_l:
+                f = open(("fieldCoords_ord%d_dim%d_%s_case%02d.txt") % 
+                (interpOrder_l[iord], dim_l[idim]+1, Qty_l[iqty][1], icase), "w")
+    
+                f.write(("%03d %d %s %03d %4.1f\n") % 
+                   (interpOrder_l[iord],
+                    dim_l[idim],
+                    Qty_l[iqty][1],
+                    nbrCells(Direction_l[idim][1], icase),
+                    spatialStep(Direction_l[idim][1], icase) ) )                   
+                   
+                iStart = physicalStartPrimal(interpOrder_l[iord])
+                iEnd   = physicalEndPrimal  (interpOrder_l[iord], Direction_l[idim][1], icase)            
+                
+                iEnd = iEnd - isDual(Qty_l[iqty][1], Direction_l[idim][1])                
 
-            f.write(("%03d %03d %s %03d %03d %03d %4.1f %4.1f %4.1f\n") % 
-               (interpOrder_l[iord],
-                nbDims_l[idim],
-                Qty_l[iqty][1],
-                nbrCellX_l[idim],
-                nbrCellY_l[idim],
-                nbrCellZ_l[idim],
-                dx_l[idim],
-                dy_l[idim],
-                dz_l[idim] ) )
-                
-            ixStart = physicalStartPrimalX(interpOrder_l[iord])
-            #ixEnd   = physicalEndPrimalX  (interpOrder_l[iord], nbrCellX_l[idim])            
-            ixEnd = allocSizeX(interpOrder_l[iord], nbrCellX_l[idim], Qty_l[iqty])
-            f.write(("ixStart = %03d \n") % (ixStart))
-            f.write(("ixEnd = %03d \n") % (ixEnd))
-                
-            for iprimal in range(ixStart, ixEnd):
-                x = fieldCoordsX(iprimal, ixStart, Qty_l[iqty], dx_l[idim], origin)
-                f.write(("%8.2f ") % (x))
-																								
-            f.close()
+                f.write(("Primal start = %03d \n") % (iStart))
+                f.write(("Primal end   = %03d \n") % (iEnd))
+                    
+                for iprimal in range(iStart, iEnd+1):
+                    x = fieldCoords(iprimal, iStart, Qty_l[iqty][1], Direction_l[idim], \
+                                    spatialStep(Direction_l[idim][1], icase), origin)
+                    f.write(("%8.2f ") % (x))
+    																								
+                f.close()
 
 
 #np.savetxt("gridlayouttest.txt", caseParamsTable, delimiter=" ", fmt="%4.1f")
