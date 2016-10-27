fc1623e595845ebca6e749ebc381a1bd97d04545
droudrou <mathieu.drouin@obspm.fr>
2016-10-27 18:21:33 +0200

    Basic classes used for projection operations

--- src/Projector/projector.h
+++ src/Projector/projector.h
@@ -1,4 +1,44 @@
 #ifndef PROJECTOR_H
 #define PROJECTOR_H
 
+
+#include <memory>
+
+#include "types.h"
+
+
+#include "grid/gridlayout.h"
+#include "Field/field.h"
+#include "Plasmas/particles.h"
+
+#include "IndexesAndWeights/indexesandweights.h"
+
+
+
+class Projector
+{
+private:
+    uint32  dim_ ;
+
+    Point  minGlobal_ ;
+
+    std::unique_ptr<IndexesAndWeights> impl_ ;
+
+public:
+
+    virtual ~Projector() ;
+
+    Projector( std::unique_ptr<IndexesAndWeights> && impl,
+                  const GridLayout & layout, const Point & minGlobal )
+        : dim_{layout.nbDimensions()}, minGlobal_{minGlobal},
+          impl_{ std::move(impl) } {}
+
+    // deposit a particle quantity onto the mesh
+    // Used to build source terms: rho, jx, jy, jz, Pxx ...
+    virtual void particleToGrid( const Field & source, const Particle & part ) const = 0 ;
+
+};
+
+
+
 #endif // PROJECTOR_H
--- src/Projector/projectorfactory.h
+++ src/Projector/projectorfactory.h
@@ -1,4 +1,81 @@
 #ifndef PROJECTORFACTORY_H
 #define PROJECTORFACTORY_H
 
+
+#include <memory>
+
+#include "types.h"
+
+#include "projector.h"
+#include "projector1d.h"
+
+#include "IndexesAndWeights/indexesandweightso1.h"
+#include "IndexesAndWeights/indexesandweightso2.h"
+#include "IndexesAndWeights/indexesandweightso3.h"
+#include "IndexesAndWeights/indexesandweightso4.h"
+
+
+
+class ProjectorFactory
+{
+
+public:
+
+    static std::unique_ptr<Projector>
+    createProjector( const GridLayout & layout, const Point & minGlobal, Direction direction )
+    {
+        uint32 order = layout.order() ;
+        uint32 dim = layout.nbDimensions() ;
+
+        double ods  = layout.odx() ;
+        double smin = minGlobal.x_ ;
+
+        switch(direction)
+        {
+        case Direction::X:
+            break;
+
+        case Direction::Y:
+            ods  = layout.ody() ;
+            smin = minGlobal.y_ ;
+            break;
+
+        case Direction::Z:
+            ods  = layout.odz() ;
+            smin = minGlobal.z_ ;
+            break;
+        }
+
+        std::unique_ptr<IndexesAndWeights> impl  ;
+        std::unique_ptr<Projector> projector ;
+
+        switch(order){
+        case 1:
+            impl = std::unique_ptr<IndexesAndWeightsO1>( new IndexesAndWeightsO1(order, ods, smin) ) ;
+            break;
+        case 2:
+            impl = std::unique_ptr<IndexesAndWeightsO2>( new IndexesAndWeightsO2(order, ods, smin) ) ;
+            break;
+        case 3:
+            impl = std::unique_ptr<IndexesAndWeightsO3>( new IndexesAndWeightsO3(order, ods, smin) ) ;
+            break;
+        case 4:
+            impl = std::unique_ptr<IndexesAndWeightsO4>( new IndexesAndWeightsO4(order, ods, smin) ) ;
+            break;
+        }
+
+        switch(dim){
+        case 1:
+            projector = std::unique_ptr<Projector1D>
+                    ( new Projector1D( std::move(impl), layout, minGlobal) ) ;
+            break;
+            // ... same for 2D and 3D
+        }
+
+        return  projector ;
+    }
+
+};
+
+
 #endif // PROJECTORFACTORY_H
