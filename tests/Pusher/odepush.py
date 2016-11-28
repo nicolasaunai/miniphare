06de43138fbf2e825609621498411aa2b84c54d2
droudrou <mathieu.drouin@obspm.fr>
2016-11-28 19:08:52 +0100

    Now, multiple integrator solutions are available: - scipy.integrate.odeint - modified Boris Pusher V1 - modified Boris Pusher V2 (retained for cpp) - experimental modified Boris

--- tests/Pusher/odepush.py
+++ tests/Pusher/odepush.py
@@ -26,6 +26,8 @@ funcDict = {'unif' : uniform,
 def main(path='./'):
 
     nbCase = 4
+    # number of Periods
+    nPer = 1
 
     x0_l  = [5.]*nbCase
     y0_l  = [0.]*nbCase
@@ -50,8 +52,8 @@ def main(path='./'):
     Tgyro_l = [2*np.pi/omega_l[ik] for ik in range(len(Bz0_l))]
 
     tbegin_l = [0.]*nbCase
-    tend_l   = [Tgyro_l[ik] for ik in range(len(Bz0_l))]
-    nstep_l  = [16, 32, 64, 32]
+    tend_l   = [nPer*Tgyro_l[ik] for ik in range(len(Bz0_l))]
+    nstep_l  = [nPer*16, nPer*32, nPer*64, nPer*32]
     
     print("T_gyro = ", tend_l)
 
@@ -132,9 +134,15 @@ def main(path='./'):
         Bzdef = funcDict[BzShape_l[icase]]
                              
 
-        sol = integrate.odeint(dynamicsEB, xv3_t0, t, \
-              args=(q, m, Ex0, Exdef, Ey0, Eydef, Ez0, Ezdef, \
-                          Bx0, Bxdef, By0, Bydef, Bz0, Bzdef ) )
+        sol = modifiedBorisV2( xv3_t0, t, q, m, \
+                   t0, tf, nstep, \
+                   Ex0, Exdef, Ey0, Eydef, Ez0, Ezdef, \
+                   Bx0, Bxdef, By0, Bydef, Bz0, Bzdef )
+
+
+#        sol_ref = integrate.odeint(dynamicsEB, xv3_t0, t, \
+#                args=(q, m, Ex0, Exdef, Ey0, Eydef, Ez0, Ezdef, \
+#                            Bx0, Bxdef, By0, Bydef, Bz0, Bzdef ) )
 
 #        print( np.size(sol[:,0]) )
 
@@ -195,6 +203,7 @@ def main(path='./'):
 #        plt.ylabel('y')
 #        plt.grid()
 #        plt.plot(sol[:, 0], sol[:, 1], '-b', label='(x, y)')
+#        plt.plot(sol_ref[:, 0], sol_ref[:, 1], '-r', label='(x, y)')
 
 #        plt.figure()
 #        plt.xlabel('t')
@@ -215,6 +224,322 @@ def main(path='./'):
 #        print(By_p)
 #        print(Bz_p)
         
+
+
+def modifiedBoris( xv3, t, q, m, \
+                   t0, tf, nstep, \
+                   Ex0, Exdef, Ey0, Eydef, Ez0, Ezdef, \
+                   Bx0, Bxdef, By0, Bydef, Bz0, Bzdef ):
+                       
+    x, y, z, vx, vy, vz = xv3
+    
+    # loop over the time steps
+    dt = (tf - t0)/nstep
+    
+    dts2 = 0.5*dt    
+    
+    sol = np.zeros((nstep+1, 6))    
+    
+    sol[0,0] = x
+    sol[0,1] = y
+    sol[0,2] = z
+    sol[0,3] = vx
+    sol[0,4] = vy
+    sol[0,5] = vz
+    
+    istep_l = np.arange(nstep)+1
+    for istep in istep_l:
+        x_tild = x + dts2*vx   
+        y_tild = y + dts2*vy
+        z_tild = z + dts2*vz
+        
+        # initializing fields
+        Ex = Exdef(Ex0, t, x)
+        Ey = Eydef(Ey0, t, x)
+        Ez = Ezdef(Ez0, t, x)
+        Bx = Bxdef(Bx0, t, x)
+        By = Bydef(By0, t, x)
+        Bz = Bzdef(Bz0, t, x)
+        
+        coef = q*dts2/m
+        
+        # 1st psuh of the electric field
+        velx1 = vx + coef*Ex
+        vely1 = vy + coef*Ey
+        velz1 = vz + coef*Ez
+        
+        # preparing magnetic rotation
+        rx = coef*Bx ;
+        ry = coef*By ;
+        rz = coef*Bz ;
+    
+        invDet = 2./(1. + rx*rx + ry*ry + rz*rz ) ;
+    
+        # preparing rotation matrix due to the magnetic field
+        # m = invDet*(I + r*r - r x I) - I where x denotes the cross product
+        mxx = invDet*( 1. + rx*rx      ) - 1. ;
+        mxy = invDet*(      rx*ry + rz ) ;
+        mxz = invDet*(      rx*rz - ry ) ;
+    
+        myx = invDet*(      ry*rx - rz ) ;
+        myy = invDet*( 1. + ry*ry      ) - 1. ;
+        myz = invDet*(      ry*rz + rx ) ;
+    
+        mzx = invDet*(      rz*rx + ry ) ;
+        mzy = invDet*(      rz*ry - rx ) ;
+        mzz = invDet*( 1. + rz*rz      ) - 1. ;
+    
+        # magnetic rotation
+        velx2 = mxx*velx1 + mxy*vely1 + mxz*velz1 ;
+        vely2 = myx*velx1 + myy*vely1 + myz*velz1 ;
+        velz2 = mzx*velx1 + mzy*vely1 + mzz*velz1 ;
+
+        # 2nd push of the electric field
+        vx = velx2 + coef*Ex
+        vy = vely2 + coef*Ey
+        vz = velz2 + coef*Ez    
+        
+        # update the position
+        x = x_tild + dts2*vx
+        y = y_tild + dts2*vy
+        z = z_tild + dts2*vz
+
+        # Update the solution table        
+        sol[istep,0] = x
+        sol[istep,1] = y
+        sol[istep,2] = z
+        sol[istep,3] = vx
+        sol[istep,4] = vy
+        sol[istep,5] = vz
+
+              
+    return sol
+
+
+
+def modifiedBorisV2( xv3, t, q, m, \
+                   t0, tf, nstep, \
+                   Ex0, Exdef, Ey0, Eydef, Ez0, Ezdef, \
+                   Bx0, Bxdef, By0, Bydef, Bz0, Bzdef ):
+                       
+    x, y, z, vx, vy, vz = xv3
+    
+    # loop over the time steps
+    dt = (tf - t0)/nstep
+    
+    dts2 = 0.5*dt    
+
+    coef = q*dts2/m 
+    
+    sol = np.zeros((nstep+1, 6))    
+    
+    sol[0,0] = x
+    sol[0,1] = y
+    sol[0,2] = z
+    sol[0,3] = vx
+    sol[0,4] = vy
+    sol[0,5] = vz
+    
+    istep_l = np.arange(nstep)+1
+    for istep in istep_l:
+        
+        # initializing fields
+        Ex = Exdef(Ex0, t, x)
+        Ey = Eydef(Ey0, t, x)
+        Ez = Ezdef(Ez0, t, x)
+        Bx = Bxdef(Bx0, t, x)
+        By = Bydef(By0, t, x)
+        Bz = Bzdef(Bz0, t, x)
+
+        # Preprush from x(tn) to prediction x(tn+1/2) 
+        x_tild = x + dts2*vx        
+        y_tild = y + dts2*vy
+        z_tild = z + dts2*vz
+        
+        # 1st psuh of the electric field
+        velx1 = vx + coef*Ex
+        vely1 = vy + coef*Ey
+        velz1 = vz + coef*Ez
+ 
+        # preparing magnetic rotation
+        rx = coef*Bx  
+        ry = coef*By 
+        rz = coef*Bz 
+        
+        rx2 = rx*rx
+        ry2 = ry*ry
+        rz2 = rz*rz        
+        rxry = rx*ry
+        rxrz = rx*rz
+        ryrz = ry*rz       
+    
+        invDet = 1./(1. + rx2 + ry2 + rz2 ) ;
+    
+        # preparing rotation matrix due to the magnetic field
+        # m = invDet*(I + r*r - r x I) - I where x denotes the cross product
+        mxx = 1. + rx2 - ry2 - rz2 
+        mxy = 2.*( rxry + rz )
+        mxz = 2.*( rxrz - ry )
+    
+        myx = 2.*( rxry - rz )
+        myy = 1. + ry2 - rx2 - rz2 
+        myz = 2.*( ryrz + rx )
+    
+        mzx = 2.*( rxrz + ry )
+        mzy = 2.*( ryrz - rx )
+        mzz = 1. + rz2 - rx2 -ry2 
+    
+        # magnetic rotation
+        velx2 = ( mxx*velx1 + mxy*vely1 + mxz*velz1 )*invDet
+        vely2 = ( myx*velx1 + myy*vely1 + myz*velz1 )*invDet
+        velz2 = ( mzx*velx1 + mzy*vely1 + mzz*velz1 )*invDet
+
+        # 2nd push of the electric field
+        vx = velx2 + coef*Ex
+        vy = vely2 + coef*Ey
+        vz = velz2 + coef*Ez    
+        
+        # update the position
+        x = x_tild + dts2*vx
+        y = y_tild + dts2*vy
+        z = z_tild + dts2*vz
+
+        # Update the solution table        
+        sol[istep,0] = x
+        sol[istep,1] = y
+        sol[istep,2] = z
+        sol[istep,3] = vx
+        sol[istep,4] = vy
+        sol[istep,5] = vz
+
+              
+    return sol
+    
+    
+    
+def modifiedBorisV3tan( xv3, t, q, m, \
+                   t0, tf, nstep, \
+                   Ex0, Exdef, Ey0, Eydef, Ez0, Ezdef, \
+                   Bx0, Bxdef, By0, Bydef, Bz0, Bzdef ):
+                       
+    x, y, z, vx, vy, vz = xv3
+    
+    # loop over the time steps
+    dt = (tf - t0)/nstep
+    
+    dts2 = 0.5*dt    
+    
+    sol = np.zeros((nstep+1, 6))    
+    
+    sol[0,0] = x
+    sol[0,1] = y
+    sol[0,2] = z
+    sol[0,3] = vx
+    sol[0,4] = vy
+    sol[0,5] = vz
+    
+    istep_l = np.arange(nstep)+1
+    for istep in istep_l:
+        
+        # initializing fields
+        Ex = Exdef(Ex0, t, x)
+        Ey = Eydef(Ey0, t, x)
+        Ez = Ezdef(Ez0, t, x)
+        Bx = Bxdef(Bx0, t, x)
+        By = Bydef(By0, t, x)
+        Bz = Bzdef(Bz0, t, x)
+
+        # Estimate of the cyclotronic pulsation
+        omega_c = q*np.sqrt( Bx**2 + By**2 + Bz**2 )/m
+        
+        # We find delta such that:
+        # tan( 0.5*omega_c*dtNew ) = 0.5*omega_c*dt
+        # dtNew = dt + delta
+        #
+        # a first order expansion gives
+        # tan(omega_c*dts2) + delta*( 1+ tan(omega_c*dts2)**2 ) = omega_c*dts2
+        #
+        # We deduce delta:
+        delta = -(omega_c*dts2 - np.tan(omega_c*dts2))/(1.+ np.tan(omega_c*dts2)**2)
+
+        delta = 0.
+        dts2 = 0.5*(dt+delta)
+        
+        if istep==1:
+            print("delta = %f" % delta)  
+        
+        coef = q*dts2/m        
+
+        # Prepush
+        x_tild = x + dts2*vx        
+        y_tild = y + dts2*vy
+        z_tild = z + dts2*vz
+        
+        # 1st psuh of the electric field
+        velx1 = vx + coef*Ex
+        vely1 = vy + coef*Ey
+        velz1 = vz + coef*Ez
+ 
+        # preparing magnetic rotation
+        # Use with delta != 0
+#        rx = coef*Bx  
+#        ry = coef*By 
+#        rz = coef*Bz 
+        
+        # Use with delta == 0
+        rx = np.tan( coef*Bx ) 
+        ry = np.tan( coef*By )
+        rz = np.tan( coef*Bz )
+        
+        rx2 = rx*rx
+        ry2 = ry*ry
+        rz2 = rz*rz        
+        rxry = rx*ry
+        rxrz = rx*rz
+        ryrz = ry*rz       
+    
+        invDet = 1./(1. + rx2 + ry2 + rz2 ) ;
+    
+        # preparing rotation matrix due to the magnetic field
+        # m = invDet*(I + r*r - r x I) - I where x denotes the cross product
+        mxx = 1. + rx2 - ry2 - rz2 
+        mxy = 2.*( rxry + rz )
+        mxz = 2.*( rxrz - ry )
+    
+        myx = 2.*( rxry - rz )
+        myy = 1. + ry2 - rx2 - rz2 
+        myz = 2.*( ryrz + rx )
+    
+        mzx = 2.*( rxrz + ry )
+        mzy = 2.*( ryrz - rx )
+        mzz = 1. + rz2 - rx2 -ry2 
+    
+        # magnetic rotation
+        velx2 = ( mxx*velx1 + mxy*vely1 + mxz*velz1 )*invDet
+        vely2 = ( myx*velx1 + myy*vely1 + myz*velz1 )*invDet
+        velz2 = ( mzx*velx1 + mzy*vely1 + mzz*velz1 )*invDet
+
+        # 2nd push of the electric field
+        vx = velx2 + coef*Ex
+        vy = vely2 + coef*Ey
+        vz = velz2 + coef*Ez    
+        
+        # update the position
+        x = x_tild + dts2*vx
+        y = y_tild + dts2*vy
+        z = z_tild + dts2*vz
+
+        # Update the solution table        
+        sol[istep,0] = x
+        sol[istep,1] = y
+        sol[istep,2] = z
+        sol[istep,3] = vx
+        sol[istep,4] = vy
+        sol[istep,5] = vz
+
+              
+    return sol    
+    
 
 
 def dynamicsEB(xv3, t, q, m, Ex0, Exdef, Ey0, Eydef, Ez0, Ezdef, \
