d0d36b09ab4933748f5f5e220b6d1801f2205755
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2017-01-18 21:01:30 +0100

    add test IndexesAndWeights to test suite code compiles OK tests all pass OK (2182 tests)

--- tests/IndexesAndWeights/computeIndexes.py
+++ tests/IndexesAndWeights/computeIndexes.py
@@ -5,10 +5,11 @@ import numpy as np
 import math
 
 import sys
-sys.path.insert(0, '../')
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)),'..'))
 
 import gridlayout
-import os
+
 
 
 
