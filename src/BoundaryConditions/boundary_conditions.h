1d06a01dac0e4479c99a0f0155b870d2450025db
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2017-02-15 14:39:21 +0100

    clean old commented out code

--- src/BoundaryConditions/boundary_conditions.h
+++ src/BoundaryConditions/boundary_conditions.h
@@ -19,35 +19,4 @@ public:
 
 
 
-/*
-class BoundaryConditions
-{
-
-private:
-    std::vector< std::unique_ptr<FieldBC> > fieldBoundaryConditions_ ;
-    std::vector< std::unique_ptr<ParticlesBC> > particlesBoundaryConditions_ ;
-
-
-public:
-    BoundaryConditions() {}
-
-    BoundaryConditions( std::vector<std::unique_ptr<FieldBC>> && fieldsBCs )
-        : fieldBoundaryConditions_{std::move(fieldsBCs)} {}
-
-    BoundaryConditions(BoundaryConditions&& toMove)      = default;
-    BoundaryConditions& operator=(BoundaryConditions&& source) = default;
-
-    ~BoundaryConditions() = default ;
-
-    std::vector< std::unique_ptr<FieldBC> > &
-    fieldBoundaryConditions() { return fieldBoundaryConditions_ ; }
-
-    void applyElectricBC( VecField & E ) ;
-    void applyMagneticBC( VecField & B ) ;
-    void applyCurrentBC ( VecField & J ) ;
-
-};*/
-
-
-
 #endif // FIELDBC3DCOLLECTION_H
