f179e471e4cac9c795764b62bdcf46bf81572f50
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2017-02-19 17:08:26 +0100

    cleaning some useless commented code

--- src/BoundaryConditions/boundary_conditions.cpp
+++ src/BoundaryConditions/boundary_conditions.cpp
@@ -7,32 +7,3 @@ BoundaryCondition::~BoundaryCondition()
 }
 
 
-#if 0
-void BoundaryConditions::applyElectricBC(VecField& E)
-{
-
-    for( auto & ptr : fieldBoundaryConditions_ )
-    {
-        ptr->applyElectricBC( E ) ;
-    }
-
-}
-
-
-void BoundaryConditions::applyMagneticBC(VecField& B)
-{
-
-    for( auto & ptr : fieldBoundaryConditions_ )
-    {
-        ptr->applyMagneticBC( B ) ;
-    }
-
-}
-
-
-void BoundaryConditions::applyCurrentBC(VecField& J)
-{
-
-
-}
-#endif
