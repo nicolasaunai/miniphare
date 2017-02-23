ca4e9dcfbab5f935a8a194c9230431c38c68212c
droudrou <mathieu.drouin@obspm.fr>
2017-02-23 20:29:11 +0100

    Simple implementation of isInBox particle selector

--- src/utilityphare.h
+++ src/utilityphare.h
@@ -52,15 +52,38 @@ class isInBox : public ParticleSelector
 {
 
 private:
-    Box box_;
+    Box parentBox_;
+    Box newBox_ ;
+
+    double dx_;
+    double dy_;
+    double dz_;
 
 public:
-    isInBox(Box& box) : box_{box}{}
+    isInBox( Box const & parentBox , Box const & newBox,
+             std::array<double, 3> dxdydz )
+        : parentBox_{parentBox}, newBox_{newBox},
+          dx_{dxdydz[0]}, dy_{dxdydz[1]},
+          dz_{dxdydz[2]} {}
 
     bool operator()(Particle const& particle) const override
     {
+        bool isIn = false;
+
+        // TODO: finish implementation of this selector
+        double posx = (particle.icell[0] + particle.delta[0]) * dx_  + parentBox_.x0 ;
+        double posy = (particle.icell[1] + particle.delta[1]) * dy_  + parentBox_.y0 ;
+        double posz = (particle.icell[2] + particle.delta[2]) * dz_  + parentBox_.z0 ;
+
         //return true if the particle is in the box
-        return false;
+        if( posx > newBox_.x0 && posx < newBox_.x1 &&
+            posy > newBox_.y0 && posy < newBox_.y1 &&
+            posz > newBox_.z0 && posz < newBox_.z1 )
+        {
+            isIn = true;
+        }
+
+        return isIn;
     }
     virtual ~isInBox(){}
 };
