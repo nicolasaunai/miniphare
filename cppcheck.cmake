c0b44a96e49aa174dde038f7e48b898379b00957
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2017-03-11 22:16:56 +0100

    change post build to custom target for cppcheck now need to run cmake cppcheck in build directory this may be improved - don’t know how for now

---
+++ cppcheck.cmake
@@ -0,0 +1,20 @@
+# additional target to perform cppcheck run, requires cppcheck
+
+# get all project files
+# HACK this workaround is required to avoid qml files checking ^_^
+file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+
+
+find_program(CPPCHECK cppcheck)
+
+add_custom_target(
+        cppcheck ALL
+        COMMAND ${CPPCHECK}
+        --enable=warning,performance,portability,information,missingInclude
+        --std=c++11
+        --library=qt.cfg
+        --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
+        --verbose
+        --quiet
+        ${ALL_SOURCE_FILES}
+)
