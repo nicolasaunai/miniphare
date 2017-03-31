ca4441a02aa13c59b7a677e6e8b824bbef117f5c
droudrou <mathieu.drouin@obspm.fr>
2017-03-31 20:21:28 +0200

    Starting to write a unit test for a single particle ... to be continued

--- tests/Splitting/test_splitting1part.cpp
+++ tests/Splitting/test_splitting1part.cpp
@@ -1,11 +1,33 @@
 
-#include "test_commons.h"
+#include <memory>
 
+#include "Plasmas/particles.h"
+
+#include "grid/gridlayout.h"
+#include "IndexesAndWeights/indexesandweights.h"
+#include "IndexesAndWeights/indexesandweightso1.h"
+#include "IndexesAndWeights/indexesandweightso2.h"
+#include "IndexesAndWeights/indexesandweightso3.h"
+#include "IndexesAndWeights/indexesandweightso4.h"
+
+#include "test_commons.h"
 #include "test_splitting1part.h"
 
 
 
 std::vector<Split1PartParams> getSplit1ParticleParamsFromFile() ;
+
+std::unique_ptr<IndexesAndWeights>  getIndexesAndWeights( uint32 const & order ) ;
+
+
+
+Split1PartParams split1ParticleInputs[] = {
+    //                dxdydz            nbrCellx y z    layout  origin             order   refNode
+    Split1PartParams({{0.1, 0., 0.}}, {{100, 1, 1}}, 1, "yee" , Point{0., 0., 0.}, 1     , 10)
+
+};
+
+
 
 
 class Split1ParticleTest: public ::testing::TestWithParam<Split1PartParams>
@@ -20,10 +42,54 @@ public:
     {
         inputs = GetParam();
 
+        // we need a layout
+        GridLayout layout{ inputs.dxdydz, inputs.nbrCells,
+                    inputs.nbDim, inputs.layout,
+                    inputs.origin, inputs.interpOrder} ;
+
+        // we need a reference node
+        uint32 refNode = inputs.referenceNode ;
+
+        // we need an IndexesAndWeights object
+        std::unique_ptr<IndexesAndWeights> indexAndWeights { getIndexesAndWeights(inputs.interpOrder) } ;
+
+        // we now initialize a Particle at the reference node
+        Particle partic(
+                    10., 1., {{refNode, 1, 1}},
+                    {{0., 0., 0.}}, {{0., 0., 0.}} );
+
+        // Now, starts the algorithm
+
 
     }
 
 };
+
+
+
+
+std::unique_ptr<IndexesAndWeights>  getIndexesAndWeights( uint32 const & order )
+{
+    std::unique_ptr<IndexesAndWeights> indexAndWeight_ptr {nullptr} ;
+
+    switch (order)
+    {
+    case 1:
+        indexAndWeight_ptr = std::unique_ptr<IndexesAndWeightsO1>( new IndexesAndWeightsO1(order) ) ;
+        break;
+    case 2:
+        indexAndWeight_ptr = std::unique_ptr<IndexesAndWeightsO1>( new IndexesAndWeightsO1(order) ) ;
+        break;
+    case 3:
+        indexAndWeight_ptr = std::unique_ptr<IndexesAndWeightsO1>( new IndexesAndWeightsO1(order) ) ;
+        break;
+    case 4:
+        indexAndWeight_ptr = std::unique_ptr<IndexesAndWeightsO1>( new IndexesAndWeightsO1(order) ) ;
+        break;
+    }
+
+    return indexAndWeight_ptr ;
+}
 
 
 
@@ -37,5 +103,5 @@ TEST_P(Split1ParticleTest, shapes)
 
 
 INSTANTIATE_TEST_CASE_P(Split1ParticleTest, Split1ParticleTest,
-                        testing::ValuesIn( getSplit1ParticleParamsFromFile() ) );
+                        testing::ValuesIn( split1ParticleInputs ) );
 
--- tests/Splitting/test_splitting1part.h
+++ tests/Splitting/test_splitting1part.h
@@ -1,11 +1,39 @@
 #ifndef TEST_SPLITTING1PART_H
 #define TEST_SPLITTING1PART_H
 
+#include <array>
+
+#include "types.h"
 
 
 struct Split1PartParams
 {
+    std::array<double, 3> dxdydz ;
+    std::array<uint32, 3> nbrCells ;
 
+    uint32 nbDim ;
+
+    std::string layout ;
+
+    Point origin{0., 0., 0.} ;
+
+    uint32 interpOrder ;
+
+    uint32 referenceNode ;
+
+
+    Split1PartParams() = default ;
+
+    Split1PartParams( std::array<double, 3> dxdydz, std::array<uint32, 3> nbrCells,
+                      uint32 nbDim, std::string layout,
+                      Point origin, uint32 interpOrder,
+                      uint32 referenceNode ):
+        dxdydz{dxdydz}, nbrCells{nbrCells},
+        nbDim{nbDim}, layout{layout},
+        origin{origin}, interpOrder{interpOrder},
+        referenceNode{referenceNode} {}
+
+    ~Split1PartParams() = default ;
 
 };
 
