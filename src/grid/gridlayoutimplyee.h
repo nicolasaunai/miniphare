32116a4b72835ae3437bc67c76f9d96a460a78e4
Thibault Payet <monwarez@gmail.com>
2017-07-03 11:31:49 +0200

    Add/Refactor function to get start and end index with HybridQuantity

--- src/grid/gridlayoutimplyee.h
+++ src/grid/gridlayoutimplyee.h
@@ -62,17 +62,28 @@ public:
 
 
     // start and end index used in computing loops
+    virtual uint32 physicalStartIndex(QtyCentering centering, Direction direction) const override;
+    virtual uint32 physicalStartIndex(HybridQuantity const& hybridQuantity,
+                                      Direction direction) const override;
     virtual uint32 physicalStartIndex(Field const& field, Direction direction) const override;
+
+    virtual uint32 physicalEndIndex(QtyCentering centering, Direction direction) const override;
+    virtual uint32 physicalEndIndex(HybridQuantity const& hybridQuantity,
+                                    Direction direction) const override;
     virtual uint32 physicalEndIndex(Field const& field, Direction direction) const override;
 
-    virtual uint32 physicalStartIndex(QtyCentering centering, Direction direction) const override;
-    virtual uint32 physicalEndIndex(QtyCentering centering, Direction direction) const override;
-
-    virtual uint32 ghostStartIndex(Field const& field, Direction direction) const override;
-    virtual uint32 ghostEndIndex(Field const& field, Direction direction) const override;
 
     virtual uint32 ghostStartIndex(QtyCentering centering, Direction direction) const override;
+    virtual uint32 ghostStartIndex(HybridQuantity const& hybridQuantity,
+                                   Direction direction) const override;
+    virtual uint32 ghostStartIndex(Field const& field, Direction direction) const override;
+
     virtual uint32 ghostEndIndex(QtyCentering centering, Direction direction) const override;
+    virtual uint32 ghostEndIndex(HybridQuantity const& hybridQuantity,
+                                 Direction direction) const override;
+    virtual uint32 ghostEndIndex(Field const& field, Direction direction) const override;
+
+
 
     virtual void deriv1D(Field const& operand, Field& derivative) const override;
 
