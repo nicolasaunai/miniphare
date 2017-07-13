afa542b3a1192b1fefac44dd56868a863010ba60
droudrou <mathieu.drouin@obspm.fr>
2017-07-13 19:46:34 +0200

    Add fake strategy to handle multi-level multi-domain patches

--- src/AMR/patchinfo.h
+++ src/AMR/patchinfo.h
@@ -6,12 +6,21 @@
 
 #include "types.h"
 
+
 struct PatchInfo
 {
     std::string pusher;
     std::vector<uint32> interpOrders;
     uint32 refinementRatio;
     std::vector<std::string> splitStrategies;
+
+    double userTimeStep; // base L0 time step
+
+    // this will help debug and tests
+    // for MLMD feature
+    std::vector<uint32> fakeStratIteration;
+    std::vector<uint32> fakeStratLevelToRefine;
+    std::vector<uint32> fakeStratPatchToRefine;
 };
 
 
