9eb9228d73e551c3185468cc8f8e3b4457ec3222
droudrou <mathieu.drouin@obspm.fr>
2017-10-23 11:28:22 +0200

    Adding operations on VecField objects: - compute variation - compute time interpolation

--- src/vecfield/vecfieldoperations.cpp
+++ src/vecfield/vecfieldoperations.cpp
@@ -17,3 +17,38 @@ void timeAverage(VecField const& vfAtTime1, VecField const& vfAtTime2, VecField&
                        Vavg.component(iComponent).begin(), [](double x) { return x * 0.5; });
     }
 }
+
+
+void getVariation(VecField const& vfAtT1, VecField const& vfAtT2, VecField& variation, double dt2t1)
+{
+    for (uint32 iComponent = 0; iComponent < 3; ++iComponent)
+    {
+        // vfAtT2 - vfAtT1
+        std::transform(vfAtT2.component(iComponent).begin(), vfAtT2.component(iComponent).end(),
+                       vfAtT1.component(iComponent).begin(),
+                       variation.component(iComponent).begin(), std::minus<double>());
+
+        // (vfAtT2 - vfAtT1)/(t2 - t1)
+        std::transform(
+            variation.component(iComponent).begin(), variation.component(iComponent).end(),
+            variation.component(iComponent).begin(), [&](double x) { return x / dt2t1; });
+    }
+}
+
+
+void timeInterpolation(VecField const& vfAtT1, VecField& variation, VecField& interpolated,
+                       double delta)
+{
+    for (uint32 iComponent = 0; iComponent < 3; ++iComponent)
+    {
+        // varprime =  delta * variation
+        std::transform(
+            variation.component(iComponent).begin(), variation.component(iComponent).end(),
+            variation.component(iComponent).begin(), [&](double x) { return x * delta; });
+
+        // vfInterp = vfAtT1 + varprime
+        std::transform(vfAtT1.component(iComponent).begin(), vfAtT1.component(iComponent).end(),
+                       variation.component(iComponent).begin(),
+                       interpolated.component(iComponent).begin(), std::plus<double>());
+    }
+}
--- src/vecfield/vecfieldoperations.h
+++ src/vecfield/vecfieldoperations.h
@@ -1,12 +1,18 @@
 #ifndef VECFIELDOPERATIONS_H
 #define VECFIELDOPERATIONS_H
 
-#include "grid/gridlayout.h"
+
 #include "vecfield/vecfield.h"
 
 
 void timeAverage(VecField const& vfAtTime1, VecField const& vfAtTime2, VecField& Vavg);
 
+
+void getVariation(VecField const& vfAtT1, VecField const& vfAtT2, VecField& variation,
+                  double dt2t1);
+
+void timeInterpolation(VecField const& vfAtT1, VecField& variation, VecField& interpolated,
+                       double delta);
 
 
 #endif // VECFIELDOPERATIONS_H
