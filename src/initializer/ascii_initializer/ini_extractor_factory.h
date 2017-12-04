dd399048ccc55aac60a513892582a93f09940f25
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2017-12-04 10:32:47 +0100

    fix the missing return statement

--- src/initializer/ascii_initializer/ini_extractor_factory.h
+++ src/initializer/ascii_initializer/ini_extractor_factory.h
@@ -1,23 +1,23 @@
 #ifndef INI_EXTRACTOR_FACTORY_H
 #define INI_EXTRACTOR_FACTORY_H
 
-#include <memory>
-#include <string>
 #include "iniextractor.h"
 #include "uniforminiextractor.h"
+#include <memory>
+#include <string>
 
 class INIExtractorFactory
 {
     using INIExtractorPtr = std::unique_ptr<INIExtractor>;
 
-public :
-
+public:
     static INIExtractorPtr createINIExtractor(std::string const& modelType)
     {
         if (modelType == "uniform")
         {
             return std::unique_ptr<INIExtractor>(new UniformINIExtractor{});
         }
+        return nullptr;
     }
 };
 
